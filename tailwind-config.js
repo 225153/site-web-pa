4597f24a4a1bd2979976da307e564f32d6e6d221
omar kharrat <omar.kharrat.ps@gmail.com>
2026-06-26 13:07:28 +0200

    security fix

---
+++ tailwind-config.js
@@ -0,0 +1,78 @@
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      "colors": {
+              "outline-variant": "#c4c6cf",
+              "on-error": "#ffffff",
+              "tertiary-container": "#003473",
+              "tertiary": "#001f4b",
+              "background": "#f8f9fa",
+              "on-secondary-container": "#646464",
+              "on-tertiary-fixed": "#001a41",
+              "on-primary-fixed": "#001b3d",
+              "surface-variant": "#e1e3e4",
+              "secondary-fixed": "#e4e2e2",
+              "inverse-surface": "#2e3132",
+              "primary-fixed": "#d6e3ff",
+              "secondary-container": "#e4e2e2",
+              "primary": "#002046",
+              "on-tertiary": "#ffffff",
+              "error": "#ba1a1a",
+              "on-tertiary-container": "#699eff",
+              "surface-tint": "#465f88",
+              "surface-container-lowest": "#ffffff",
+              "outline": "#74777f",
+              "on-secondary": "#ffffff",
+              "surface-container-high": "#e7e8e9",
+              "primary-container": "#1b365d",
+              "surface-container-low": "#f3f4f5",
+              "on-tertiary-fixed-variant": "#004493",
+              "secondary": "#5e5e5e",
+              "on-primary-container": "#87a0cd",
+              "error-container": "#ffdad6",
+              "tertiary-fixed": "#d8e2ff",
+              "on-surface": "#191c1d",
+              "tertiary-fixed-dim": "#adc7ff",
+              "secondary-fixed-dim": "#c8c6c6",
+              "surface-container-highest": "#e1e3e4",
+              "primary-fixed-dim": "#aec7f7",
+              "on-secondary-fixed-variant": "#474747",
+              "inverse-primary": "#aec7f7",
+              "on-primary": "#ffffff",
+              "on-primary-fixed-variant": "#2e476f",
+              "on-background": "#191c1d",
+              "surface-bright": "#f8f9fa",
+              "on-surface-variant": "#44474e",
+              "surface-container": "#edeeef",
+              "on-error-container": "#93000a",
+              "surface": "#f8f9fa",
+              "surface-dim": "#d9dadb",
+              "inverse-on-surface": "#f0f1f2",
+              "on-secondary-fixed": "#1b1c1c"
+      },
+      "borderRadius": {
+              "DEFAULT": "0.125rem",
+              "lg": "0.25rem",
+              "xl": "0.5rem",
+              "full": "0.75rem"
+      },
+      "spacing": {},
+      "fontFamily": {
+              "headline": [
+                      "Montserrat"
+              ],
+              "display": [
+                      "Montserrat"
+              ],
+              "body": [
+                      "Inter"
+              ],
+              "label": [
+                      "Inter"
+              ]
+      },
+      "fontSize": {}
+},
+  },
+}
